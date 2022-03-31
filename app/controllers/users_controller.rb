4494a3834c138aead7f4d37032a3d63a31fba10a
cgalatro2 <chasegalatro@gmail.com>
2022-03-30 17:58:57 -0700

    added user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'Welcome to Tokenhood'
+      redirect_to 'http://localhost:3000/tokens'
+    else
+      render 'new'
+      redirect_to 'http://localhost:3000/tokens'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 },
                     format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
